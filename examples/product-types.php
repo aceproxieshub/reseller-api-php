71760fa5f91563a3fd3fb1f9308167f30a889da0
AceProxies <support@aceproxies.com>
2026-08-11 15:05:09 +0200

    docs: examples for products

---
+++ examples/product-types.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use Aceproxies\ResellerApi\Client;
+
+/** @var Client $client */
+$client = require __DIR__ . '/_bootstrap.php';
+
+$types = $client->products()->types();
+
+var_dump($types);
---
+++ examples/products-list.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use Aceproxies\ResellerApi\Client;
+
+/** @var Client $client */
+$client = require __DIR__ . '/_bootstrap.php';
+
+$products = $client->products()->list();
+
+var_dump($products);
