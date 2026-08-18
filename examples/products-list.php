57d8f72979a12c984a8ab7e5762ad2532841acdf
AceProxies <support@aceproxies.com>
2026-08-18 11:53:14 +0200

    fix: spelling of a product type in the related example

--- examples/products-list.php
+++ examples/products-list.php
@@ -7,6 +7,6 @@ use Aceproxies\ResellerApi\Client;
 /** @var Client $client */
 $client = require __DIR__ . '/_bootstrap.php';
 
-$products = $client->products()->list('residential');
+$products = $client->products()->list('residential_proxy');
 
 var_dump($products);
